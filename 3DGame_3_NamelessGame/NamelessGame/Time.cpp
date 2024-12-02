9741cda4c4994f7d14706bde08f74ec03aefc9c4
kakisawa <uk9im3@outlook.jp>
2024-12-02 16:44:20 +0900

    12/02

--- 3DGame_3_NamelessGame/NamelessGame/Time.cpp
+++ 3DGame_3_NamelessGame/NamelessGame/Time.cpp
@@ -19,11 +19,13 @@ namespace {
 	};
 
 	constexpr int kMinutes = 3600;
-	constexpr float kSecondTen = 600.0f;
-	constexpr float kSecondOne = 60.0f;
+	constexpr int kSecondOne = 60;
 }
 
 Time::Time():
+	m_timeHandleMinites(-1),
+	m_timeHandleSecondsTen(-1),
+	m_timeHandleSecondsOne(-1),
 	m_second(kMaxtimeLimit),
 	m_isTimeUp(false)
 {
@@ -32,6 +34,7 @@ Time::Time():
 	{
 		m_numberUIHandle[i] = LoadGraph(kNumberUI[i]);
 	}
+	m_timeBgHandle = LoadGraph("Data/Image/SceneGame/時間/Time.png");
 }
 
 Time::~Time()
@@ -46,6 +49,14 @@ void Time::Update()
 {
 	m_second--;
 
+	// 表示用時間計算
+	m_minutes = m_second / kMinutes;
+	m_secondsTen = ((m_second % kMinutes) / kSecondOne) / 10;
+	m_secondsOne= ((m_second % kMinutes) / kSecondOne) % 10;
+
+	SetTimeHandle();
+
+
 	// 制限時間が0になったら
 	if (m_second <= 0.0f)
 	{
@@ -58,13 +69,20 @@ void Time::Draw()
 	DrawFormatString(0, 1000, 0xffffff, "m_second=%.2f", m_second/60);
 
 	DrawFormatString(0, 1020, 0xffffff,
-		"残り時間:分=%d", m_second / kMinutes);
+		"残り時間:分=%d", m_minutes);
 
 	DrawFormatString(0, 1040, 0xffffff,
-		"残り時間:十秒=%d", ((m_second % kMinutes) / 60) / 10);
+		"残り時間:十秒=%d", m_secondsTen);
 
 	DrawFormatString(0, 1060, 0xffffff,
-		"残り時間:秒=%d", ((m_second % kMinutes) / 60) % 10);
+		"残り時間:秒=%d", m_secondsOne);
+
+	DrawGraph(10, 20, m_timeBgHandle, true);
+
+	DrawGraph(35, 45, m_timeHandleMinites, true);
+	DrawGraph(90, 50, m_numberUIHandle[10], true);
+	DrawGraph(110, 45, m_timeHandleSecondsTen, true);
+	DrawGraph(155, 45, m_timeHandleSecondsOne, true);
 }
 
 void Time::End()
@@ -74,6 +92,8 @@ void Time::End()
 	{
 		DeleteGraph(m_numberUIHandle[i]);
 	}
+
+	DeleteGraph(m_timeBgHandle);
 }
 
 void Time::Calculation()
@@ -81,4 +101,104 @@ void Time::Calculation()
 	
 
 
+}
+
+void Time::SetTimeHandle()
+{
+	if (m_minutes == 1)
+	{
+		m_timeHandleMinites = m_numberUIHandle[1];
+	}
+	else if (m_minutes == 2)
+	{
+		m_timeHandleMinites = m_numberUIHandle[2];
+	}
+	else if (m_minutes == 3)
+	{
+		m_timeHandleMinites = m_numberUIHandle[3];
+	}
+
+
+	if (m_secondsTen == 0)
+	{
+		m_timeHandleSecondsTen = m_numberUIHandle[0];
+	}
+	else if (m_secondsTen == 1)
+	{
+		m_timeHandleSecondsTen = m_numberUIHandle[1];
+	}
+	else if (m_secondsTen == 2)
+	{
+		m_timeHandleSecondsTen = m_numberUIHandle[2];
+	}
+	else if (m_secondsTen == 3)
+	{
+		m_timeHandleSecondsTen = m_numberUIHandle[3];
+	}
+	else if (m_secondsTen == 4)
+	{
+		m_timeHandleSecondsTen = m_numberUIHandle[4];
+	}
+	else if (m_secondsTen == 5)
+	{
+		m_timeHandleSecondsTen = m_numberUIHandle[5];
+	}
+	else if (m_secondsTen == 6)
+	{
+		m_timeHandleSecondsTen = m_numberUIHandle[6];
+	}
+	else if (m_secondsTen == 7)
+	{
+		m_timeHandleSecondsTen = m_numberUIHandle[7];
+	}
+	else if (m_secondsTen == 8)
+	{
+		m_timeHandleSecondsTen = m_numberUIHandle[8];
+	}
+	else if (m_secondsTen == 9)
+	{
+		m_timeHandleSecondsTen = m_numberUIHandle[9];
+	}
+
+
+	if (m_secondsOne == 0)
+	{
+		m_timeHandleSecondsOne = m_numberUIHandle[0];
+	}
+	else if (m_secondsOne == 1)
+	{
+		m_timeHandleSecondsOne = m_numberUIHandle[1];
+	}
+	else if (m_secondsOne == 2)
+	{
+		m_timeHandleSecondsOne = m_numberUIHandle[2];
+	}
+	else if (m_secondsOne == 3)
+	{
+		m_timeHandleSecondsOne = m_numberUIHandle[3];
+	}
+	else if (m_secondsOne == 4)
+	{
+		m_timeHandleSecondsOne = m_numberUIHandle[4];
+	}
+	else if (m_secondsOne == 5)
+	{
+		m_timeHandleSecondsOne = m_numberUIHandle[5];
+	}
+	else if (m_secondsOne == 6)
+	{
+		m_timeHandleSecondsOne = m_numberUIHandle[6];
+	}
+	else if (m_secondsOne == 7)
+	{
+		m_timeHandleSecondsOne = m_numberUIHandle[7];
+	}
+	else if (m_secondsOne == 8)
+	{
+		m_timeHandleSecondsOne = m_numberUIHandle[8];
+	}
+	else if (m_secondsOne == 9)
+	{
+		m_timeHandleSecondsOne = m_numberUIHandle[9];
+	}
 }
--- 3DGame_3_NamelessGame/NamelessGame/Time.h
+++ 3DGame_3_NamelessGame/NamelessGame/Time.h
@@ -34,7 +34,15 @@ public:
 	/// </summary>
 	void End();
 
+	/// <summary>
+	/// 秒数の計算
+	/// </summary>
 	void Calculation();
+
+	/// <summary>
+	/// 秒数にあたる画像をセットする
+	/// </summary>
+	void SetTimeHandle();
 
 	/// <summary>
 	/// 制限時間終了フラグを渡す
@@ -45,10 +53,14 @@ public:
 private:
 
 	int m_minutes;		// 分数一桁の値
-	int m_seconds;		// 秒数十桁の値
-	int m_minutes;		// 秒数一桁の値
+	int m_secondsTen;	// 秒数十桁の値
+	int m_secondsOne;	// 秒数一桁の値
 
+	int m_timeHandleMinites;
+	int m_timeHandleSecondsTen;
+	int m_timeHandleSecondsOne;
 
+	int m_timeBgHandle;
 	std::array<int, 11>m_numberUIHandle{};	// 時間のUI画像
 
 	int m_second;		// 残りのゲーム秒数(制限時間)*60
